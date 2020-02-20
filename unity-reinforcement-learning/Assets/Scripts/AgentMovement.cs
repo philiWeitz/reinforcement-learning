f8578e1bf05a8a86a982e6314c1fd7679fae2bc0
Philipp Weitz <philiweitz@gmail.com>
2020-02-20 23:34:02 +0200

    initial commit

---
+++ unity-reinforcement-learning/Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float gravity = 20.0f;
+
+    private int isNotGroundedCount;
+
+    private Vector3 initialPosition;
+
+    private Quaternion initialRotation;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        initialPosition = this.transform.position;
+        initialRotation = this.transform.rotation;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MoveAgent(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void MoveAgent(float horizontal, float vertical)
+    {
+        if (!characterController.isGrounded)
+        {
+            isNotGroundedCount += 1;
+        } else
+        {
+            isNotGroundedCount = 0;
+        }
+
+        if (isNotGroundedCount < 2)
+        {
+            if (characterController.isGrounded)
+            {
+                // We are grounded, so recalculate
+                // move direction directly from axes
+                moveDirection = new Vector3(0.0f, 0.0f, vertical);
+                moveDirection *= speed;
+            }
+
+            Vector3 rotation = new Vector3(0, horizontal * 90 * Time.deltaTime, 0);
+            this.transform.Rotate(rotation);
+
+            // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+            // as an acceleration (ms^-2)
+            moveDirection.y -= gravity * Time.deltaTime;
+            moveDirection = this.transform.TransformDirection(moveDirection);
+
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+        else
+        {
+            this.transform.rotation = initialRotation;
+            this.transform.position = initialPosition;
+            isNotGroundedCount = 0;
+        }
+    }
+}
